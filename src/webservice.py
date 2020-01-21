d3015314644ff41ff19057d38421d44410adb137
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-01-21 15:06:22 +0000

    feat: Add webservice

---
+++ src/webservice.py
@@ -0,0 +1,44 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ''
+
+@app.route('/predict')
+def predict():
+    from flask import request
+    from datetime import datetime, timedelta
+    from model import predict_demand
+    import json
+
+    api_key = request.args['api_key']
+    model_fname = request.args.get('model_fname', 'gaussian_process')
+    data_dir = request.args.get('data_dir', 'data')
+    date = request.args.get('date', datetime.now() + timedelta(days = 1))
+
+    if isinstance(date, datetime): date = datetime.strftime(date, '%Y-%m-%d')
+
+    prediction = predict_demand(
+        date = date,
+        api_key = api_key,
+        model_fname = model_fname,
+        data_dir = data_dir,
+        return_std = True
+    )
+
+    if prediction != prediction: 
+        error = 'We cannot predict more than nine days away!'
+        result = {'date': date, 'error': error}
+    else:
+        if isinstance(prediction, tuple):
+            prediction, std = prediction
+            result = {'date': date, 'prediction': prediction, 'std': std}
+        else:
+            result = {'date': date, 'prediction': prediction}
+    
+    return json.dumps(result)
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0')
