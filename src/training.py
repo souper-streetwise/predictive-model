9e10f338bfa687ec685aed4f1eb06f88fd717fab
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-01-31 16:06:26 +0000

    feat: Enable not saving model and/or log

--- src/training.py
+++ src/training.py
@@ -1,37 +1,28 @@
 def train_model(X, y, n_iter: int = 100, cv: int = 10, data_dir: str = 'data', 
-    workers: int = -1, eps: float = 1e-7) -> dict:
+    workers: int = -1, save_log: bool = True, save_model: bool = True, 
+    model_name: str = 'soup_model', **kwargs) -> dict:
     from skopt import BayesSearchCV
     from skopt.space import Real, Categorical, Integer
-    import pickle
     import warnings
-    import pandas as pd
-    from modules import pExtraTreesRegressor, TQDM 
-    from utils import get_path
+    from model import pExtraTreesRegressor
+    from utils import get_path, TQDM
 
     hyperparams = {
-        'n_estimators': Integer(10, 5000),
-        'max_depth': Integer(10, 1000),
-        'min_samples_split': Real(eps, 1.0, prior = 'uniform'),
-        'min_samples_leaf': Real(eps, 0.5, prior = 'uniform'),
+        'n_estimators': Integer(10, 10000),
         'max_features': Categorical(['auto', 'sqrt', 'log2']),
-        'max_leaf_nodes': Integer(2, 10000),
-        'min_impurity_decrease': Real(0.0, 1.0, prior = 'uniform'),
-        'bootstrap': Categorical([True, False]),
-        'max_samples': Real(eps, 1 - eps, prior = 'uniform')
+        'bootstrap': Categorical([True, False])
     }
 
-    estimator = pExtraTreesRegressor(criterion = 'mae', n_jobs = workers)
+    search = BayesSearchCV(
+        estimator = pExtraTreesRegressor(criterion = 'mae', n_jobs = workers), 
+        search_spaces = hyperparams, 
+        n_iter = n_iter,
+        scoring = 'neg_mean_absolute_error',
+        cv = cv,
+        n_jobs = workers
+    )
 
-    with TQDM(total = n_iter, desc = 'Optimising ExtraTrees') as pbar:
-        search = BayesSearchCV(
-            estimator = estimator, 
-            search_spaces = hyperparams, 
-            n_iter = n_iter,
-            scoring = 'neg_mean_absolute_error',
-            cv = cv,
-            n_jobs = workers,
-        )
-
+    with TQDM(total = n_iter, desc = 'Optimising model') as pbar:
         with warnings.catch_warnings():
             message = 'The objective has been evaluated at this point before.'
             warnings.filterwarnings('ignore', message = message)
@@ -49,17 +40,21 @@ def train_model(X, y, n_iter: int = 100, cv: int = 10, data_dir: str = 'data',
         'feat_imps': feat_imps
     }
 
-    results = pd.DataFrame(search.cv_results_)
-    results_path = get_path(data_dir) / 'extra_trees_training_log.csv'
-    results.to_csv(results_path, index = False)
+    if save_model:
+        import pickle
+        with open(get_path(data_dir) / model_name, 'wb') as f:
+            pickle.dump(model_data, f)
 
-    with open(get_path(data_dir) / 'extra_trees', 'wb') as f:
-        pickle.dump(model_data, f)
+    if save_log:
+        import pandas as pd
+        results = pd.DataFrame(search.cv_results_)
+        results_path = get_path(data_dir) / f'{model_name}_training_log.csv'
+        results.to_csv(results_path, index = False)
 
     return model_data, results
 
 if __name__ == '__main__':
-    from utils import get_path
+    from utils import get_path, boolean
     from data import get_data
     from argparse import ArgumentParser
 
@@ -68,7 +63,14 @@ if __name__ == '__main__':
     parser.add_argument('--cv', type = int, default = 10)
     parser.add_argument('--workers', type = int, default = -1)
     parser.add_argument('--data_dir', default = 'data')
+    parser.add_argument('--save_log', type = boolean, default = True)
+    parser.add_argument('--save_model', type = boolean, default = True)
+    parser.add_argument('--model_name', default = 'soup_model')
+    parser.add_argument('--include_month', type = boolean, default = True)
+    parser.add_argument('--include_day_of_week', type = boolean, default=True)
+    parser.add_argument('--include_day_of_month', type = boolean, default=True)
     args = vars(parser.parse_args())
 
-    model_data, results = train_model(*get_data(), **args)
+    X, y = get_data(**args)
+    model_data, results = train_model(X, y, **args)
     print(model_data)
